ba0c63db66a604d99779949e1a18c9f4bd25f79e
Duarte Pardal <duarte.pardal@tecnico.ulisboa.pt>
2025-01-12 17:14:48 +0000

    Add test for for loop unfolding
    Co-authored-by: Luís Marques <luis.p.marques@tecnico.ulisboa.pt>
    Co-authored-by: Manuel Navalho <manuel.navalho@tecnico.ulisboa.pt>

---
+++ T05-08/for_loop_unfolding.js
@@ -0,0 +1,13 @@
+a = b('username');
+c = "";
+d = "";
+for (; e == "";) {
+    c = d;
+    if (x == 42) {
+        d = a;
+    }
+    a = s(a + 1);
+}
+q = z(c);
+
+// Adapted from slices/5-loops/5b-loops-unfolding.js
