fc5c062195083c33cfb911f785d09fbde60f621d
Duarte Pardal <duarte.pardal@tecnico.ulisboa.pt>
2025-01-12 17:02:52 +0000

    Add test for for loops
    Co-authored-by: Luís Marques <luis.p.marques@tecnico.ulisboa.pt>
    Co-authored-by: Manuel Navalho <manuel.navalho@tecnico.ulisboa.pt>

---
+++ T05-05/for_loop_1.js
@@ -0,0 +1,14 @@
+a = b('hello');
+c = d('world');
+i = "";
+for (let j = 0; j < 5; j++) {
+    f = s(c, 0, 1);
+
+    if (f == "a") {
+        i = i + "'";
+    } else {
+        i = i + " ";
+    }
+}
+z(0, i);
+
